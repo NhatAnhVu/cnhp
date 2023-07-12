461e9c5b08af22c68b30ee269e663eef3bd0f8ea
Thang Van <85693841+UnbertenKma@users.noreply.github.com>
2023-07-12 23:40:23 +0700

    page_Admin_12/7

--- src/pages/Administrator/Genaral/Genaral.js
+++ src/pages/Administrator/Genaral/Genaral.js
@@ -1,20 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Card } from 'antd';
-import { Button, Dropdown } from 'antd';
+import { Button, Dropdown, Modal } from 'antd';
 import {  CalendarOutlined } from '@ant-design/icons';
 import { Col, Divider, Row } from 'antd';
 import img1 from '../../../common/images/imageAdministrator_page/tongbaoicon1.svg'
 import { Dots } from '../../../styles';
-import { Wapper } from './style';
-
-const style = {
-  padding: '8px 0',
-  background: 'rgb(255, 255, 255)',
-  boxShadow: 'rgba(21, 67, 152, 0.1) 0px 0px 30px',
-  borderRadius: '10px',
-  width: '100%',
-  height: '150px',
-  borderTop: '6px solid red'
+import { Wapper, WapperModal } from './style';
+import { AudioOutlined } from '@ant-design/icons';
+import { Input, Space } from 'antd';
+const { Search } = Input;
+const suffix = (
+  <AudioOutlined
+    style={{
+      fontSize: 16,
+      color: '#1677ff',
+    }}
+  />
+);
+const style = (borderColor) => {
+  return {
+    padding: '8px 0',
+    background: 'rgb(255, 255, 255)',
+    boxShadow: 'rgba(21, 67, 152, 0.1) 0px 0px 30px',
+    borderRadius: '10px',
+    width: '100%',
+    height: '150px',
+    borderTop: `6px solid ${borderColor}`,
+  }
 };
 
 const items = [
@@ -45,6 +57,26 @@ const items = [
 ];
 
 const Genaral = () => {
+
+  const [open, setOpen] = useState(false);
+  const [confirmLoading, setConfirmLoading] = useState(false);
+  const [modalText, setModalText] = useState('Content of the modal');
+  const showModal = () => {
+    setOpen(true);
+  };
+  const handleOk = () => {
+    setModalText('The modal will be closed after two seconds');
+    setConfirmLoading(true);
+    setTimeout(() => {
+      setOpen(false);
+      setConfirmLoading(false);
+    }, 2000);
+  };
+  const handleCancel = () => {
+    console.log('Clicked cancel button');
+    setOpen(false);
+  };
+
   return (
     <Wapper>
       <Dropdown
@@ -81,8 +113,10 @@ const Genaral = () => {
             lg: 32,
           }}
         >
-          <Col className="gutter-row" span={8}>
-            <div style={style}>
+          <Col className="gutter-row" span={8}
+            onClick={showModal}
+          >
+            <div style={style('red')}>
               <Row>
                 <Col span={16} style={{marginTop: '14px'}}>
                   <Row >
@@ -102,10 +136,40 @@ const Genaral = () => {
             </div>
           </Col>
           <Col className="gutter-row" span={8}>
-            <div style={style}>col-4</div>
+            <div style={style('#154398')}><Row>
+                <Col span={16} style={{marginTop: '14px'}}>
+                  <Row >
+                    <div style={{display: 'flex'}}>
+                      <Dots style={{marginLeft: '20px', marginTop: '8px'}}/>
+                      <span style={{marginLeft: '10px', fontWeight: 600, fontSize: '14px', }}> SỐ KHÁCH HÀNG MỚI</span>  
+                    </div>
+                  </Row>
+                  <Row>
+                    <span style={{margin: '20px 50px', fontWeight: 600, fontSize: '30px', color: '#154398'}}>12</span>
+                  </Row>
+                </Col>
+                <Col span={8} style={{display: 'flex', alignItems: 'center'}}>
+                  <img src={img1}/>
+                </Col>
+              </Row></div>
           </Col>
           <Col className="gutter-row" span={8}>
-            <div style={style}>col-4</div>
+            <div style={style('#F88C00')}><Row>
+                <Col span={16} style={{marginTop: '14px'}}>
+                  <Row >
+                    <div style={{display: 'flex'}}>
+                      <Dots style={{marginLeft: '20px', marginTop: '8px'}}/>
+                      <span style={{marginLeft: '10px', fontWeight: 600, fontSize: '14px', }}> SỐ KHÁCH HÀNG ĐANG QUẢN LÝ</span>  
+                    </div>
+                  </Row>
+                  <Row>
+                    <span style={{margin: '20px 50px', fontWeight: 600, fontSize: '30px', color: '#154398'}}>12</span>
+                  </Row>
+                </Col>
+                <Col span={8} style={{display: 'flex', alignItems: 'center'}}>
+                  <img src={img1}/>
+                </Col>
+              </Row></div>
           </Col>
           
         </Row>
@@ -120,7 +184,7 @@ const Genaral = () => {
           style={{marginTop: '40px'}}
         >
           <Col className="gutter-row" span={8}>
-            <div style={style}>
+            <div style={style('#00C590')}>
               <Row>
                 <Col span={16} style={{marginTop: '14px'}}>
                   <Row >
@@ -140,15 +204,73 @@ const Genaral = () => {
             </div>
           </Col>
           <Col className="gutter-row" span={8}>
-            <div style={style}>col-4</div>
+            <div style={style('#F88C00')}><Row>
+                <Col span={16} style={{marginTop: '14px'}}>
+                  <Row >
+                    <div style={{display: 'flex'}}>
+                      <Dots style={{marginLeft: '20px', marginTop: '8px'}}/>
+                      <span style={{marginLeft: '10px', fontWeight: 600, fontSize: '14px', }}> YÊU CẦU HỖ TRỢ</span>  
+                    </div>
+                  </Row>
+                  <Row>
+                    <span style={{margin: '20px 50px', fontWeight: 600, fontSize: '30px', color: '#154398'}}>12</span>
+                  </Row>
+                </Col>
+                <Col span={8} style={{display: 'flex', alignItems: 'center'}}>
+                  <img src={img1}/>
+                </Col>
+              </Row></div>
           </Col>
           <Col className="gutter-row" span={8}>
-            <div style={style}>col-4</div>
+            <div style={style('#ED1117')}><Row>
+                <Col span={16} style={{marginTop: '14px'}}>
+                  <Row >
+                    <div style={{display: 'flex'}}>
+                      <Dots style={{marginLeft: '20px', marginTop: '8px'}}/>
+                      <span style={{marginLeft: '10px', fontWeight: 600, fontSize: '14px', }}> SỐ KHÁCH HÀNG NGỪNG DỊCH VỤ SỬ DỤNG NƯỚC</span>  
+                    </div>
+                  </Row>
+                  <Row>
+                    <span style={{margin: '20px 50px', fontWeight: 600, fontSize: '30px', color: '#154398'}}>12</span>
+                  </Row>
+                </Col>
+                <Col span={8} style={{display: 'flex', alignItems: 'center'}}>
+                  <img src={img1}/>
+                </Col>
+              </Row></div>
           </Col>
           
         </Row>
           </>
       </Card>
+    
+      <WapperModal title="Danh sách hóa đơn chưa thanh toán"
+        open={open}
+        onOk={handleOk}
+        confirmLoading={confirmLoading}
+        onCancel={handleCancel}
+        >
+          <Space direction="vertical">
+            <Search
+              placeholder="input search text"
+              // onSearch={onSearch}
+              style={{
+                width: 200,
+              }}
+            />
+          </Space>
+
+          <Card
+          title=""
+          bordered={false}
+          style={{
+            width: '100%',
+            justifyContent: 'left'
+          }}
+          >
+              1 cái table
+          </Card>
+      </WapperModal>
     </Wapper>
   )
 }
--- src/pages/Administrator/Genaral/style.js
+++ src/pages/Administrator/Genaral/style.js
@@ -1,8 +1,20 @@
+import { Modal } from 'antd'
 import styled from 'styled-components'
 
 export const Wapper = styled.div`
     .ant-col {
         padding-left: -16px;
         padding-right: -16px;
+    }
+
+    
+
+`
+
+export const WapperModal = styled(Modal)`
+    .ant-modal-content {
+        .ant-modal-footer {
+            display: none ;
+        }
     }
 `

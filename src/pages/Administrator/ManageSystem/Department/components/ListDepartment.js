69d2fed36f860391975a694d82562e166a7d2c95
hoangquan2012 <88789218+hoangquan2012@users.noreply.github.com>
2023-07-25 01:54:16 +0700

    update

--- src/pages/Administrator/ManageSystem/Department/components/ListDepartment.js
+++ src/pages/Administrator/ManageSystem/Department/components/ListDepartment.js
@@ -1,65 +1,72 @@
-import { Tree } from 'antd';
-import React from 'react';
-const treeData = [
-    {
-        title: 'parent 1',
-        key: '0-0',
-        children: [
-            {
-                title: 'parent 1-0',
-                key: '0-0-0',
-                children: [
-                    {
-                        title: 'leaf',
-                        key: '0-0-0-0'
-                    },
-                    {
-                        title: 'leaf',
-                        key: '0-0-0-1'
-                    }
-                ]
-            },
-            {
-                title: 'parent 1-1',
-                key: '0-0-1',
-                children: [
-                    {
-                        title: (
-                            <span
-                                style={{
-                                    color: '#1677ff'
-                                }}
-                            >
-                                sss
-                            </span>
-                        ),
-                        key: '0-0-1-0'
-                    }
-                ]
-            }
-        ]
-    }
-];
+import { Table, Tree } from "antd";
+import React, { useEffect, useState } from "react";
+
+import { useSelector, useDispatch } from "react-redux";
+import { fetchDepartment, fetchDepartmentParent } from "../../../../../reducers/departmentSlice";
+const buildTreeData = (data) => {
+  const treeData = [];
+  data?.forEach((item) => {
+    treeData.push({
+      key: item.DepartmentID,
+      title: item.DepartmentName,
+      children: [], // Add children if needed
+    });
+  });
+  return treeData;
+};
 function ListDepartment() {
-    const onSelect = (selectedKeys, info) => {
-        console.log('selected', selectedKeys, info);
-    };
-    return (
-        <>
-            <Tree
-                defaultExpandedKeys={['0-0-0', '0-0-1']}
-                defaultSelectedKeys={['0-0-0', '0-0-1']}
-                defaultCheckedKeys={['0-0-0', '0-0-1']}
-                onSelect={onSelect}
-                treeData={treeData}
-                style={{
-                    borderRadius: 10,
-                    border: '1px solid var(--border, #DDD)',
-                    background: 'var(--white, #FFF)'
-                }}
-            />
-        </>
-    );
+  const dispatch = useDispatch();
+  const onSelect = (selectedKeys, info) => {
+    console.log("selected", selectedKeys, info);
+  };
+
+  const [dataListView, setdataListView] = useState([]);
+  const [dataListParentView, setdataListparentView] = useState([]);
+  const listView = useSelector(
+    (state) => state?.department?.department?.departmentGet?.Object
+  );
+
+  const listViewParent = useSelector(
+    (state) => state?.department?.departmentParent?.departmentParentGet?.Object
+  );
+
+  const getList = () => {
+    dispatch(fetchDepartment());
+  };
+  const getparentList = () => {
+    dispatch(fetchDepartmentParent());
+  };
+  useEffect(() => {
+    setdataListView(listView);
+  }, [listView]);
+  useEffect(() => {
+    getList();
+  }, []);
+
+  useEffect(() => {
+    setdataListparentView(listViewParent);
+  }, [listViewParent]);
+  useEffect(() => {
+    getparentList();
+  }, []);
+
+  const treeData = buildTreeData(dataListView);
+  return (
+    <>
+      <Tree
+        defaultExpandedKeys={["0-0-0", "0-0-1"]}
+        defaultSelectedKeys={["0-0-0", "0-0-1"]}
+        defaultCheckedKeys={["0-0-0", "0-0-1"]}
+        onSelect={onSelect}
+        treeData={treeData}
+        style={{
+          borderRadius: 10,
+          border: "1px solid var(--border, #DDD)",
+          background: "var(--white, #FFF)",
+        }}
+      />
+    </>
+  );
 }
 
 export default ListDepartment;
--- src/services/apis/department.js
+++ src/services/apis/department.js
@@ -3,3 +3,7 @@ import axiosInstance from '../axios';
 export const GetDepartment = () => {
     return axiosInstance.get('Department/getAll');
 };
+
+export const GetDepartmentParent = () => {
+    return axiosInstance.get('Department/getAllParent');
+};
